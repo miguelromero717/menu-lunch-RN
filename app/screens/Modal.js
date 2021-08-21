1a0e800085f644558d80e930a45cb46d33ff2e03
Miguel Angel Romero <miguelromero717@gmail.com>
2021-08-21 18:53:05 -0500

    Add modal information with Order - Add buttons to accept or cancel Order

--- app/screens/Modal.js
+++ app/screens/Modal.js
@@ -1,10 +1,48 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import useFetch from "../hooks/useFetch";
 
-export default () => {
-    return (
-        <View>
-            <Text>Soy Modal</Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default ({ navigation }) => {
+  const id = navigation.getParam("_id");
+  const { loading, data } = useFetch(
+    `https://menu-lunch-api-miguelromero717.vercel.app/api/meals/${id}`
+  );
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text>{data._id}</Text>
+          <Text>{data.name}</Text>
+          <Text>{data.description}</Text>
+          <Button title="Accept" onPress={() => {
+            fetch(`https://menu-lunch-api-miguelromero717.vercel.app/api/orders`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                meal_id: id,
+                user_id: '112233'
+              })
+            }).then(() => {
+              alert('Order generated successfully')
+              navigation.navigate('Meals')
+            })
+          }}/>
+          <Button title="Cancel" onPress={() => navigation.navigate('Meals')}/>
+        </>
+      )}
+    </View>
+  );
+};
